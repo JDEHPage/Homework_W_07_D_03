65038316d9a3ae2655ebccc08630b0117d0b1015
John Page <jdehpage@gmail.com>
2019-02-20 16:24:07 +0000

    Completed forex converter with extensions

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+      el: '#app',
+      data: {
+        baseInfo: [],
+        exchangeRates: [],
+        selectedCurrencyBase: null,
+        selectedCurrencyExchange: null,
+        amount: null,
+        calculatedValue: 0,
+
+
+
+      },
+      mounted(){
+        this.getForex();
+      },
+      methods: {
+        getForex: function() {
+          fetch('https://api.exchangeratesapi.io/latest')
+          .then(response => response.json())
+          .then(data => this.baseInfo = data)
+          .then(data => this.exchangeRates = data.rates)
+        },
+      calculate: function() {
+        this.calculatedValue = 1/this.exchangeRates[this.selectedCurrencyBase] * this.amount * this.exchangeRates[this.selectedCurrencyExchange]
+        this.calculatedValue = Math.round (this.calculatedValue * 100)/100
+      }
+
+
+
+
+      }
+
+  })
+})
